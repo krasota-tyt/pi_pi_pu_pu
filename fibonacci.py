83a11f79c8c7dc67632adc410fc2962e78f20cdf
Vlad <krasota.vlad15@gmail.com>
2023-06-06 23:01:16 +0300

    first commit

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
+    return fib_sequence
+
+numbers = fibonacci(40)
+for number in numbers:
+    print(number)
